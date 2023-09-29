78f012f894302e84555c4be6cfe8df7214c298ef
Amy Sawyer <amyleighsawyer97@gmail.com>
2023-09-29 11:53:56 +0000

    added in a contact page

--- assets/sendEmails.js
+++ assets/sendEmails.js
@@ -2,7 +2,7 @@ function sendMail(contactForm) {
     emailjs.send("gmail", "Pizza-berra", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
+        "booking_request": contactForm.booking_request.value
     })
     .then(
         function(response) {
